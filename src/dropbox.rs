d45c2f10706bd15cf2cb7cf88b708631e84c481b
Mark Lussier <mlussier@gmail.com>
2022-11-26 11:33:14 -0700

    moar refactor

--- src/dropbox.rs
+++ src/dropbox.rs
@@ -13,40 +13,57 @@ use std::{
 const PATH_MAESTRAL: &str = "/Library/Application Support/maestral/maestral.ini";
 const PATH_DROPBOX: &str = "/.dropbox/host.db";
 
-pub fn get_folder() -> Option<String> {
-    let homedir = dirs::home_dir().unwrap().display().to_string();
-
-    let maestral_path = format!("{}{}", homedir, PATH_MAESTRAL);
-
-    let dbx = format!("{}{}", homedir, PATH_DROPBOX);
-
-    if Path::new(&dbx).exists() {
-        let file = File::open(dbx).expect("no such file");
-        let buf = BufReader::new(file);
-        let lines: Vec<String> = buf
-            .lines()
-            .map(|l| l.expect("Could not parse line"))
-            .collect();
-
-        let bytes = decode(lines[1].as_bytes()).unwrap();
-        Some(String::from_utf8(bytes).unwrap())
-    } else if Path::new(&maestral_path).exists() {
-        let conf = Ini::load_from_file(maestral_path).unwrap();
-        let section = conf.section(Some("sync")).unwrap();
-        let path = section.get("path").unwrap();
-
-        Some(path.to_string())
-    } else {
-        None
-    }
-
-    // Are they using maestral? they should be
+pub struct DropBox {
+    pub path: String,
 }
 
-pub fn get_path_last_part(path: &str, sep: char) -> String {
-    let pieces = path.split(sep);
-    match pieces.last() {
-        Some(p) => p.into(),
-        None => path.into(),
+impl DropBox {
+    pub fn new() -> Self {
+        Self {
+            path: "".to_string(),
+        }
+    }
+
+    pub fn folder(&mut self) -> String {
+        if self.path.is_empty() {
+            self.path = self.get_folder();
+        }
+        self.path.clone()
+    }
+
+    fn get_folder(&self) -> String {
+        let homedir = dirs::home_dir().unwrap().display().to_string();
+
+        let maestral_path = format!("{}{}", homedir, PATH_MAESTRAL);
+
+        let dbx = format!("{}{}", homedir, PATH_DROPBOX);
+
+        if Path::new(&dbx).exists() {
+            let file = File::open(dbx).expect("no such file");
+            let buf = BufReader::new(file);
+            let lines: Vec<String> = buf
+                .lines()
+                .map(|l| l.expect("Could not parse line"))
+                .collect();
+
+            let bytes = decode(lines[1].as_bytes()).unwrap();
+
+            String::from_utf8(bytes).unwrap()
+        } else if Path::new(&maestral_path).exists() {
+            let conf = Ini::load_from_file(maestral_path).unwrap();
+            let section = conf.section(Some("sync")).unwrap();
+            let path = section.get("path").unwrap();
+
+            path.to_string()
+        } else {
+            "".to_string()
+        }
+
+        // Are they using maestral? they should be
+    }
+
+    pub fn name(&self) -> String {
+        let pieces = self.path.split('/');
+        pieces.last().unwrap().to_string()
     }
 }
